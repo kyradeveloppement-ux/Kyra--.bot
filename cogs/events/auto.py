7d34af9faff9f230db814367e2aebaff10b689e4
kyradeveloppement-ux <kyradeveloppement@gmail.com>
2025-09-09 06:49:45 +1100

    Update auto.py

--- cogs/events/auto.py
+++ cogs/events/auto.py
@@ -16,14 +16,14 @@ class Autorole(Cog):
         async for entry in guild.audit_logs(limit=3):
             if entry.action == discord.AuditLogAction.bot_add:
                 embed = discord.Embed(
-                    description=f"<:mail:1299664322225635338> **Thanks for adding me.**\n\n<:red_arrow:1247226071783440404> My default prefix is `$`\n<:red_arrow:1247226071783440404> Use the `$help` command to see a list of commands\n<:red_arrow:1247226071783440404> For detailed guides, FAQ and information, visit our **[Support Server](https://discord.gg/odx)**",
+                    description=f"⛓️‍💥 **Thanks for adding me.**\n\n🌐 My default prefix is `+`\n👏 Use the `+help` command to see a list of commands\n⛓️‍💥 For detailed guides, FAQ and information, visit our **[Support Server](https://discord.gg/X9NDdMf3xf)**",
                     color=0xff0000
                 )
                 embed.set_thumbnail(url=entry.user.avatar.url if entry.user.avatar else entry.user.default_avatar.url)
                 embed.set_author(name=f"{guild.name}", icon_url=guild.me.display_avatar.url)
-                embed.set_footer(text="Powered by Olympus Development™", icon_url="https://cdn.discordapp.com/icons/699587669059174461/f689b4366447d5a23eda8d0ec749c1ba.png")
-                website_button = Button(label='Website', style=discord.ButtonStyle.link, url='https://olyumpus.vercel.app/')
-                support_button = Button(label='Support', style=discord.ButtonStyle.link, url='https://discord.gg/odx')
+                embed.set_footer(text="Powered by Kyra✨ Development™❤️", icon_url="https://cdn.discordapp.com/icons/699587669059174461/f689b4366447d5a23eda8d0ec749c1ba.png")
+                website_button = Button(label='Website', style=discord.ButtonStyle.link, url='https://discord.gg/X9NDdMf3xf')
+                support_button = Button(label='Support', style=discord.ButtonStyle.link, url='https://discord.gg/X9NDdMf3xf')
                 vote_button = Button(label='Vote for Me', style=discord.ButtonStyle.link, url=f'https://top.gg/bot/{self.bot.user.id}/vote')
                 view = View()
                 view.add_item(support_button)
